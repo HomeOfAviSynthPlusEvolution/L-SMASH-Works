77a0549161712747690bbb7b21c0d8069f77c7c5
enccc <enccc.github@gmail.com>
2018-03-30 19:47:37 +0900

    Tweak AviSynth C-API for GCC

--- include/avs/capi.h
+++ include/avs/capi.h
@@ -39,14 +39,10 @@
 #  define EXTERN_C
 #endif
 
-#ifdef MSVC
 #ifndef AVSC_USE_STDCALL
 #  define AVSC_CC __cdecl
 #else
 #  define AVSC_CC __stdcall
-#endif
-#else
-#  define AVSC_CC
 #endif
 
 #define AVSC_INLINE static __inline
