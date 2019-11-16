HomeOfAviSynthPlusEvolution/L-SMASH-Works

commit a4e1f54a01bc9c181bf57eb977590313931b3f29

VideoSource: Improve capability check in prefer_hw

common/decode.c
@@ -33,7 +33,7 @@ extern "C"
 #include "decode.h"
 #include "qsv.h"
 
-static AVCodec *select_hw_decoder( const char *codec_name, const int prefer_hw_decoder )
+static AVCodec *select_hw_decoder( const char *codec_name, const int prefer_hw_decoder, AVCodecParameters *codecpar )
 {
     char hw_decoder_name[32] = { 0 };
     const size_t codec_name_length = strlen( codec_name );
@@ -46,7 +46,8 @@ static AVCodec *select_hw_decoder( const char *codec_name, const int prefer_hw_d
     AVCodecContext *ctx = avcodec_alloc_context3( hw_decoder );
     if( !ctx )
         return NULL;
-    if( avcodec_open2( ctx, hw_decoder, NULL ) < 0
+    if( avcodec_parameters_to_context( ctx, codecpar ) < 0
+     || avcodec_open2( ctx, hw_decoder, NULL ) < 0
      || avcodec_send_packet( ctx, NULL ) < 0 )
     {
         avcodec_free_context( &ctx );
@@ -58,9 +59,10 @@ static AVCodec *select_hw_decoder( const char *codec_name, const int prefer_hw_d
 
 const AVCodec *find_decoder
 (
-    enum AVCodecID  codec_id,
-    const char    **preferred_decoder_names,
-    const int       prefer_hw_decoder
+    enum AVCodecID     codec_id,
+    AVCodecParameters *codecpar,
+    const char       **preferred_decoder_names,
+    const int          prefer_hw_decoder
 )
 {
     AVCodec *codec = avcodec_find_decoder( codec_id );
@@ -89,12 +91,12 @@ const AVCodec *find_decoder
         AVCodec *preferred_decoder;
         if( prefer_hw_decoder == 3 )
         {
-            preferred_decoder = select_hw_decoder( codec_name, 1 );
+            preferred_decoder = select_hw_decoder( codec_name, 1, codecpar );
             if( !preferred_decoder )
-                preferred_decoder = select_hw_decoder( codec_name, 2 );
+                preferred_decoder = select_hw_decoder( codec_name, 2, codecpar );
         }
         else
-            preferred_decoder = select_hw_decoder( codec_name, prefer_hw_decoder );
+            preferred_decoder = select_hw_decoder( codec_name, prefer_hw_decoder, codecpar );
         if( preferred_decoder )
             codec = preferred_decoder;
     }
@@ -148,7 +150,7 @@ int find_and_open_decoder
     const int        thread_count
 )
 {
-    const AVCodec *codec = find_decoder( stream->codecpar->codec_id, preferred_decoder_names, prefer_hw_decoder );
+    const AVCodec *codec = find_decoder( stream->codecpar->codec_id, stream->codecpar, preferred_decoder_names, prefer_hw_decoder );
     if( !codec )
         return -1;
     return open_decoder( ctx, stream, codec, thread_count );

common/libavsmash.c
@@ -445,7 +445,12 @@ static const AVCodec *libavsmash_find_decoder
         /* Try to get any valid codec_id from summaries. */
         for( uint32_t i = 0; i < config->count && codec_id == AV_CODEC_ID_NONE; i++ )
             codec_id = get_codec_id_from_description( config->entries[i].summary );
-    return find_decoder( codec_id, config->preferred_decoder_names, config->prefer_hw_decoder );
+    AVCodecParameters *codecpar = avcodec_parameters_alloc();
+    if( codecpar )
+        avcodec_parameters_from_context( codecpar, config->ctx );
+    const AVCodec *codec = find_decoder( codec_id, codecpar, config->preferred_decoder_names, config->prefer_hw_decoder );
+    avcodec_parameters_free( &codecpar );
+    return codec;
 }
 
 int libavsmash_find_and_open_decoder
@@ -866,7 +871,7 @@ void update_configuration
     config->ctx->opaque = NULL;
     avcodec_free_context( &config->ctx );
     /* Find an appropriate decoder. */
-    const AVCodec *codec = find_decoder( config->queue.codec_id, config->preferred_decoder_names, config->prefer_hw_decoder );
+    const AVCodec *codec = find_decoder( config->queue.codec_id, codecpar, config->preferred_decoder_names, config->prefer_hw_decoder );
     if( !codec )
     {
         strcpy( error_string, "Failed to find the decoder.\n" );

VapourSynth/README
@@ -99,7 +99,7 @@
         [LWLibavSource]
             LWLibavSource(string source, int stream_index = -1, int threads = 0, int cache = 1, string cachefile = source + ".lwi",
                           int seek_mode = 0, int seek_threshold = 10, int dr = 0, int fpsnum = 0, int fpsden = 1, 
-                          int variable = 0, string format = "", int repeat = 0, int dominance = 1, string decoder = "", int prefer_hw = 0)
+                          int variable = 0, string format = "", int repeat = 0, int dominance = 0, string decoder = "", int prefer_hw = 0)
                 * This function uses libavcodec as video decoder and libavformat as demuxer.
             [Arguments]
                 + source

common/decode.h
@@ -30,9 +30,10 @@ static inline uint32_t get_decoder_delay
 
 const AVCodec *find_decoder
 (
-    enum AVCodecID  codec_id,
-    const char    **preferred_decoder_names,
-    const int       prefer_hw_decoder
+    enum AVCodecID     codec_id,
+    AVCodecParameters *codecpar,
+    const char       **preferred_decoder_names,
+    const int          prefer_hw_decoder
 );
 
 int open_decoder

common/lwlibav_dec.c
@@ -93,7 +93,7 @@ void lwlibav_update_configuration
     avcodec_free_context( &dhp->ctx );
     /* Find an appropriate decoder. */
     const lwlibav_extradata_t *entry = &exhp->entries[extradata_index];
-    const AVCodec *codec = find_decoder( entry->codec_id, dhp->preferred_decoder_names, dhp->prefer_hw_decoder );
+    const AVCodec *codec = find_decoder( entry->codec_id, codecpar, dhp->preferred_decoder_names, dhp->prefer_hw_decoder );
     if( !codec )
     {
         strcpy( error_string, "Failed to find the decoder.\n" );

commit 87d01f9dff7177f9fcd4d651ccda53cddbcb9bcb

LibavSMASH: Fix access violation
Regression in https://github.com/HolyWu/L-SMASH-Works/commit/a4e1f54a01bc9c181bf57eb977590313931b3f29

common/decode.c
@@ -47,7 +47,7 @@ static AVCodec *select_hw_decoder( const char *codec_name, const int prefer_hw_d
     AVCodecContext *ctx = avcodec_alloc_context3( hw_decoder );
     if( !ctx )
         return NULL;
-    if( avcodec_parameters_to_context( ctx, codecpar ) < 0
+    if( (codecpar && avcodec_parameters_to_context( ctx, codecpar ) < 0)
      || avcodec_open2( ctx, hw_decoder, NULL ) < 0
      || avcodec_send_packet( ctx, NULL ) < 0 )
     {

common/libavsmash.c
@@ -445,12 +445,7 @@ static const AVCodec *libavsmash_find_decoder
         /* Try to get any valid codec_id from summaries. */
         for( uint32_t i = 0; i < config->count && codec_id == AV_CODEC_ID_NONE; i++ )
             codec_id = get_codec_id_from_description( config->entries[i].summary );
-    AVCodecParameters *codecpar = avcodec_parameters_alloc();
-    if( codecpar )
-        avcodec_parameters_from_context( codecpar, config->ctx );
-    const AVCodec *codec = find_decoder( codec_id, codecpar, config->preferred_decoder_names, config->prefer_hw_decoder );
-    avcodec_parameters_free( &codecpar );
-    return codec;
+    return find_decoder( codec_id, NULL, config->preferred_decoder_names, config->prefer_hw_decoder );
 }
 
 int libavsmash_find_and_open_decoder